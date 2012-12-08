3eeb8952c6776cea74b3bbeb93b42c9c5282ead9
rumpelart <rumpelart@gmx.de>
2012-12-08 20:19:24 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Direktkreditverwaltung::Application.config.secret_token = 'a383ee3ac49f1ad66acb8368bf1d1e5758151f9b22fe8a3de8024d0ff204841fa06eec6d773b0e839e21941ed3e553c0b2081f2e5579247fd8a064bfb1dbbd6f'
