20477b49edca1451aaec024de0922e14bb212a74
rumpelart <rumpelart@gmx.de>
2012-12-14 11:16:47 +0100

    account balance

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,5 +1,13 @@
 class Contract < ActiveRecord::Base
+  # contract representing one account
+
   belongs_to :contact
   has_many :accounting_entries
   attr_accessible :duration_month, :duration_years, :interest_rate, :number, :start, :category, :comment
-end
+
+  #account balance for given date
+  def balance date=DateTime.now.to_date
+    entries = accounting_entries.where("date <= ?", date)
+    entries.inject(0) {|sum, entry| sum + entry[:amount]}
+  end
+end 
