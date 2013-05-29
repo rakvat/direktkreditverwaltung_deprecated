3afb42709cd26578af5df8a9e6da08d0dd4742f9
Michael Prilop <Michael.Prilop@gmail.com>
2013-05-29 16:10:32 +0200

    made contract_version more robust (checking interest rate to be sane value)

--- app/models/contract_version.rb
+++ app/models/contract_version.rb
@@ -1,4 +1,13 @@
 class ContractVersion < ActiveRecord::Base
   belongs_to :contract
   attr_accessible :duration_months, :duration_years, :interest_rate, :start, :version
+
+  validates_presence_of :interest_rate, :start, :version
+
+  def interest_rate=(interest)
+    interest = interest.to_f
+    raise "Interest should be between 0.00 and 0.09" unless (0.00 .. 0.09).cover?(interest)
+    write_attribute(:interest_rate, interest)
+  end
+
 end
